180eaf2167fb8bb1435b57b58851539a15c0a3aa
ashrafuddin1496 <asraf1496@gmail.com>
2020-12-25 11:58:34 +0600

    dashboard for different user access done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,21 @@
         <div class="col-md-8">
             <h2>Dashboard</h2>
             <ul class="list-group">
+
+                @if(in_array('User',json_decode(Auth::user() ->role ->permission)))
                 <li class="list-group-item"><a href="{{route('user.index')}}">Users</a></li>
+                @endif
+
+                @if(in_array('Role',json_decode(Auth::user() ->role ->permission)))
                 <li class="list-group-item"><a href="{{route('role.index')}}">Role</a></li>
+                    @endif
+
+                    @if(in_array('Settings',json_decode(Auth::user() ->role ->permission)))
                 <li class="list-group-item"><a href="">Settings</a></li>
+                    @endif
+
                 <li class="list-group-item"><a href="">Seo</a></li>
+
             </ul>
         </div>
     </div>
